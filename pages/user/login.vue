92ac103763abfb20775cac641545ae4f8092a8e9
jasmine <1491246034@qq.com>
2020-02-09 19:57:50 +0800

    完成注册页

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -19,17 +19,19 @@
         <LoginForm v-if="currentTab == 0" />
 
         <!-- 注册功能组件 -->
-        <!-- <RegisterForm v-if="currentTab == 1"/> -->
+        <RegisterForm v-if="currentTab == 1" />
       </div>
     </el-row>
   </div>
 </template>
 
 <script>
-import LoginForm from '@/components/user/LoginForm.vue'
+import LoginForm from "@/components/user/LoginForm.vue";
+import RegisterForm from "@/components/user/RegisterForm.vue";
 export default {
   components: {
-    LoginForm
+    LoginForm,
+    RegisterForm
   },
   data() {
     return {
